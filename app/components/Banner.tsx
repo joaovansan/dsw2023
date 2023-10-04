c77d95d035a15e2e109f4f0b5f67101e8abc8db6
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-04 15:39:37 +0000

    fix Cor das informações

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,7 +6,7 @@ export default function Banner() {
     <img src="/img/banner.svg" alt="Descrição da Imagem" />
   </div>
   
-  <div className="basis-1/2 text-center text-white">
+  <div className="basis-1/2 text-center text-[#00e77f]">
     <h2>UTFTEC</h2>
     <h3>A MELHOR ESCOLA DE INFORMÁTICA</h3>
     <a
